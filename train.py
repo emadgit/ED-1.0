538396b29d423990c06d5d4cc85a0c67bca32d05
Emad Dehnavi <dehnavi@gmx.com>
2025-05-04 11:32:51 +0200

    using dataset batch

--- train.py
+++ train.py
@@ -12,25 +12,42 @@ print("Tokenizer loaded successfully.")
 print(f"Tokenizer vocab size: {tokenizer.vocab_size}")
 
 
-def tokenize_function(examples):
-    print("Tokenizing examples...")
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+print(f"Tokenizer pad token: {tokenizer.pad_token}")
+print(f"Tokenizer pad token id: {tokenizer.pad_token_id}")
+print(f"Tokenizer padding side: {tokenizer.padding_side}")
 
-    print(examples)
+
+def tokenize_function(batch):
+    print("Tokenizing batch...")
+
+    print(batch)
     # Tokenize the input text
 
-    single_prompt = f"### Question:\n{examples['prompt'][0]}\n\n### Answer:\n{examples['response'][0]}"
-    # Tokenize the single prompt
-    tokenized_example = tokenizer(single_prompt, return_tensors="pt")
+    prompts = [
+        f"###Question:\n {p}\n###Answer:\n {a}"
+        for p, a in zip(batch["prompt"], batch["response"])
+    ]
 
-    token_ids = tokenized_example["input_ids"].squeeze(0).tolist()
+    # Tokenize the prompts
+    tokenize_new_rows = tokenizer(
+        prompts,
+        max_length=512,
+        padding="max_length",
+        truncation=True,
+    )
+    token_ids = tokenize_new_rows["input_ids"]
+    attention_mask = tokenize_new_rows["attention_mask"]
 
     return {
         "input_ids": token_ids,
-        "attention_mask": tokenized_example["attention_mask"][0].squeeze(0).tolist(),
+        "attention_mask": attention_mask,
+        "labels": token_ids,
     }
 
 
-dataset = dataset.map(tokenize_function)
+dataset = dataset.map(tokenize_function, batched=True)
 print("Dataset tokenized successfully.")
 print(f"Number of tokenized examples: {len(dataset)}")
 print(f"First tokenized example: {dataset[0]}")
